10e4440697e1b0040a3ffe1994ad5e5ace682ab9
rosenthj <rosenth0@purdue.edu>
2023-12-29 06:21:49 -0500

    Added RelNetA

--- src/model.py
+++ src/model.py
@@ -220,8 +220,70 @@ class NetRelH(nn.Module):
         # return
         buffer = bytearray()
         self._s(buffer, self.c1, "conv layer", bias=False, verbose=verbose)
+        buffer.extend(tensor_to_bytes(self.b1.data))
+        self._s(buffer, self.out, "out", verbose=verbose)
+        self._s(buffer, self.f1, "f1 layer", verbose=verbose)
+        self._s(buffer, self.fout, "f out", bias=False, verbose=verbose)
+        with open(filename, "wb") as f:
+            f.write(buffer)
+
+
+class NetRelA(nn.Module):
+    def __init__(self, d=8, num_inputs=772, activation=F.relu):
+        super().__init__()
+        self.d = d
+        self.activation = activation
+        self.c1 = nn.Conv2d(12, 12 * d, 15, padding=7, bias=False)
+        self.b1 = nn.parameter.Parameter(data=torch.zeros((12 * d, 8, 8)))
+        self.a1 = nn.Conv2d(12 * d, 1, 1)
+        # conv, no bias, probably 15x15
+        # linear for non-board visible, with bias
+        # filter
+        # out, 3 8x8 conv filters
+        self.out = nn.Conv2d(12 * d, 3, 8, padding=0)
+
+    def forward(self, x_in, activate=True):
+        x = x_in[:, :768].view(-1, 12, 8, 8)
+        mask = torch.repeat_interleave(x, self.d, dim=1)
+        x = self.c1(x) + self.b1
+        x = x * mask
+        x = self.activation(x)
+        a = self.a1(x)
+        a = F.softmax(a.view(-1, 1, 8*8), dim=-1).view(-1, 1, 8, 8)
+        x = a * x
+        x = self.out(x)
+        x = torch.squeeze(x, 3)
+        x = torch.squeeze(x, 2)
+        if not activate:
+            return x
+        return F.softmax(x, dim=-1)
+
+    def f(self, x_in):
+        x = x_in[:, :768].view(-1, 12, 8, 8)
+        mask = torch.repeat_interleave(x, self.d, dim=1)
+        x = self.c1(x) + self.b1
+        # x = self.b1
+        return x * mask
+
+    def _s(self, buffer, l, name, bias=True, verbose=0):
+        if l is None:
+            return
+        if verbose >= 1:
+            print(f"Buffering {name}")
+        buffer.extend(tensor_to_bytes(l.weight))
+        if bias:
+            if verbose >= 2:
+                print(f"{name} has bias")
+            buffer.extend(tensor_to_bytes(l.bias))
+
+    def serialize(self, filename, verbose=0):
+        # print(f"Skipping serialize call. Not yet implemented!")
+        # return
+        buffer = bytearray()
+        self._s(buffer, self.c1, "conv layer", bias=False, verbose=verbose)
         # self._s(buffer, self.b1, "bias layer", bias=False, verbose=verbose)
         buffer.extend(tensor_to_bytes(self.b1.data))
         self._s(buffer, self.out, "out", verbose=verbose)
+        self._s(buffer, self.a1, "att layer", bias=False, verbose=verbose)
         with open(filename, "wb") as f:
             f.write(buffer)
