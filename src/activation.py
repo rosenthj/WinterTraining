50fc4150e12fd1532404dde2ce0cd4ad0b36d383
rosenthj <rosenth0@purdue.edu>
2024-07-29 15:28:18 -0400

    SCReLU v SF added

--- src/activation.py
+++ src/activation.py
@@ -12,6 +12,16 @@ class SqCReLU(nn.Module):
         return torch.pow(torch.clip(x, self.clip_min, self.clip_max), 2.0)
 
 
+class SqCReLUvSF(nn.Module):
+    def __init__(self, clip_min=0, clip_max=1):
+        super(SqCReLUvSF, self).__init__()
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+
+    def forward(self, x):
+        return torch.clip(torch.pow(x, 2.0), self.clip_min, self.clip_max)
+
+
 class MishSqrt(nn.Module):
     def __init__(self):
         super(MishSqrt, self).__init__()
